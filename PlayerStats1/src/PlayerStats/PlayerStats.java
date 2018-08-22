e9291c3e6220de96e66830e3d7dfdf0624e4fba5
Justin Noland <JustinNoland24@yahoo.com>
2018-08-22 15:26:08 -0400

    Players names and Scores

---
+++ PlayerStats1/src/PlayerStats/PlayerStats.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PlayerStats;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Justi
+ */
+public class PlayerStats {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         String[] names=new String[80];
+       int[] scores=new int[80];
+       
+       readingNamesAndScores(names, scores, args[0]);
+       
+        // TODO code application logic here
+    }
+    
+    private static void readingNamesAndScores(String[] names, int[] scores, String filename){
+        
+        try {
+            Scanner reader = new Scanner(new File(filename));
+            
+            int currentIndex =0;
+            while(reader.hasNext()){
+               String [] line= reader.nextLine().split(" ");
+            
+               names[currentIndex]= line[0];
+               scores[currentIndex]= Integer.parseInt(line[1]);
+               
+               currentIndex++;
+               
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(PlayerStats.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("File could not be found in readingNamesAndScores");
+        }
+    }
+}
